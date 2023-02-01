44d33b9bf2da0d3739ee679899b2baf72f4bb6ae
Miftakhurrokhman <miftakhurrokhman8@gmail.com>
2023-02-01 21:57:09 +0700

    Create job.py

---
+++ job.py
@@ -0,0 +1,15 @@
+
+import sched
+import time
+import subprocess
+
+def run_app():
+    # Code for the function you want to schedule
+    subprocess.call(["python3", "get_snmp.py"])
+    print("Running the job...")
+
+s = sched.scheduler(time.time, time.sleep)
+
+while True:
+    s.enter(21600, 1, run_app)
+    s.run()
